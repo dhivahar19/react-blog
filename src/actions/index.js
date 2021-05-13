9babe5cb879997396509d40ce41632b3bf12aa4d
dhivaharChinnathampi <dhivahar.chinnathampi@gmail.com>
2021-05-13 21:52:14 +0200

    ready for deploy

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import jsonplaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash'
+
+export const fetchPosts = () =>  async dispatch => {
+        const response = await jsonplaceholder.get('/posts');
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response.data
+        })
+}
+// non memoized version
+/* export const fetchUser = (id) =>  async dispatch => {
+    const response = await jsonplaceholder.get(`/users/${id}`);
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+} */
+// memoized version
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) =>{
+    const response = await jsonplaceholder.get(`/users/${id}`);
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+})
